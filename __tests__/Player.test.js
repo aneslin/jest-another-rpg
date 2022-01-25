badc75ac487705e12cbbe608c6cf1079928361e7
Aaron Neslin <aneslin125@gmail.com>
2022-01-24 22:41:51 -0500

    still not  quite right

--- __tests__/Player.test.js
+++ __tests__/Player.test.js
@@ -2,7 +2,7 @@ const Potion = require('../lib/Potion');
 
 jest.mock('../lib/Potion.js');
 
-console.log(new Potion());
+console.log(new Potion('health'));
 const Player = require('../lib/Player');
 
 
@@ -17,7 +17,10 @@ test('create a player object', () => {
     expect(player.health).toEqual(expect.any(Number));
     expect(player.strength).toEqual(expect.any(Number));
     expect(player.agility).toEqual(expect.any(Number));
+    expect(player.inventory).toEqual(
     expect.arrayContaining([expect.any(Object)])
+);
+
 
     
 
